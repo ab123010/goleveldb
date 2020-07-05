1cdbdb677856cc20e66bdfd3e947b5583f860f03
ab123010 <wangkaiyang99@163.com>
2020-07-05 15:44:38 +0800

    20-7-5

--- leveldb/memdb/memdb.go
+++ leveldb/memdb/memdb.go
@@ -30,24 +30,26 @@ type dbIter struct {
 	util.BasicReleaser
 	p          *DB
 	slice      *util.Range
-	node       int
-	forward    bool
+	node       int			// 指向DB中跳表nodeData的位置索引，node == 0指向虚拟头结点，此时迭代器不可用
+	forward    bool			// true: findGE; false: findLT
 	key, value []byte
 	err        error
 }
 
+// 从DB中取出目前迭代的键值对，若指定相应参数验证范围
 func (i *dbIter) fill(checkStart, checkLimit bool) bool {
 	if i.node != 0 {
 		n := i.p.nodeData[i.node]
 		m := n + i.p.nodeData[i.node+nKey]
 		i.key = i.p.kvData[n:m]
 		if i.slice != nil {
+			// 检验是否位于slice范围中
 			switch {
 			case checkLimit && i.slice.Limit != nil && i.p.cmp.Compare(i.key, i.slice.Limit) >= 0:
 				fallthrough
 			case checkStart && i.slice.Start != nil && i.p.cmp.Compare(i.key, i.slice.Start) < 0:
 				i.node = 0
-				goto bail
+				goto bail	// 位于范围外
 			}
 		}
 		i.value = i.p.kvData[m : m+i.p.nodeData[i.node+nVal]]
@@ -107,6 +109,7 @@ func (i *dbIter) Seek(key []byte) bool {
 	i.p.mu.RLock()
 	defer i.p.mu.RUnlock()
 	if i.slice != nil && i.slice.Start != nil && i.p.cmp.Compare(key, i.slice.Start) < 0 {
+		// 若小于范围区间，设置为key为区间左值
 		key = i.slice.Start
 	}
 	i.node, _ = i.p.findGE(key, false)
@@ -172,6 +175,7 @@ func (i *dbIter) Release() {
 }
 
 const (
+	// 从nodeData中取node数据
 	nKV = iota
 	nKey
 	nVal
@@ -185,35 +189,39 @@ type DB struct {
 	rnd *rand.Rand
 
 	mu     sync.RWMutex
-	kvData []byte
-	// Node data:
+	kvData []byte				// 仅存键值对，用node的偏移量提取，添加直接放到尾部
+	// Node data:	跳表中node
 	// [0]         : KV offset
 	// [1]         : Key length
 	// [2]         : Value length
-	// [3]         : Height
-	// [3..height] : Next nodes
-	nodeData  []int
-	prevNode  [tMaxHeight]int
-	maxHeight int
-	n         int
-	kvSize    int
+	// [3]         : Height		跳表有几层
+	// [3..height] : Next nodes	指向该层下一个节点
+	nodeData  []int				// 跳表，有虚拟头结点
+	prevNode  [tMaxHeight]int	// 当前节点各层的前一节点
+	maxHeight int				// 跳表最高层级数
+	n         int				// 存储的键值对数量
+	kvSize    int				// 存储的所有键值对[]byte长度
 }
 
 func (p *DB) randHeight() (h int) {
 	const branching = 4
 	h = 1
 	for h < tMaxHeight && p.rnd.Int()%branching == 0 {
+		// 概率为随机int为4的倍数
 		h++
 	}
 	return
 }
 
 // Must hold RW-lock if prev == true, as it use shared prevNode slice.
+// 寻找等于key或大于key的最小的node的位置索引
+// prev == true时需要RW-lock，此时使用共享的prevNode slice
 func (p *DB) findGE(key []byte, prev bool) (int, bool) {
 	node := 0
+	// 从最高层开始
 	h := p.maxHeight - 1
 	for {
-		next := p.nodeData[node+nNext+h]
+		next := p.nodeData[node+nNext+h]	// +nNext到各层指针位置，+h指定第几层指针
 		cmp := 1
 		if next != 0 {
 			o := p.nodeData[next]
@@ -221,21 +229,26 @@ func (p *DB) findGE(key []byte, prev bool) (int, bool) {
 		}
 		if cmp < 0 {
 			// Keep searching in this list
+			// next.key小于当前key，本层向后寻找，移到本层下一个node
 			node = next
 		} else {
 			if prev {
+				// 记录目标node该层前一节点
 				p.prevNode[h] = node
 			} else if cmp == 0 {
+				// 找到目标值
 				return next, true
 			}
 			if h == 0 {
-				return next, cmp == 0
+				return next, cmp == 0	// 返回最终值，true: next.key == key; false: 大于key的最小node的位置索引
 			}
+			// 去下一层寻找
 			h--
 		}
 	}
 }
 
+// 寻找小于key的最大node位置索引
 func (p *DB) findLT(key []byte) int {
 	node := 0
 	h := p.maxHeight - 1
@@ -243,23 +256,26 @@ func (p *DB) findLT(key []byte) int {
 		next := p.nodeData[node+nNext+h]
 		o := p.nodeData[next]
 		if next == 0 || p.cmp.Compare(p.kvData[o:o+p.nodeData[next+nKey]], key) >= 0 {
+			// next.key >= key; next == 0表示为该层最后一个节点
 			if h == 0 {
 				break
 			}
 			h--
 		} else {
+			// next.key < key
 			node = next
 		}
 	}
 	return node
 }
 
+// 跳表最后一个节点
 func (p *DB) findLast() int {
 	node := 0
 	h := p.maxHeight - 1
 	for {
 		next := p.nodeData[node+nNext+h]
-		if next == 0 {
+		if next == 0 {	// 该层最后一个节点
 			if h == 0 {
 				break
 			}
@@ -275,18 +291,20 @@ func (p *DB) findLast() int {
 // for that key; a DB is not a multi-map.
 //
 // It is safe to modify the contents of the arguments after Put returns.
+// 设置键值对，若key已存在会重新，不是key的一对多映射
 func (p *DB) Put(key []byte, value []byte) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 
 	if node, exact := p.findGE(key, true); exact {
+		// key已存在于跳表，新的键值对放到kvData尾部
 		kvOffset := len(p.kvData)
 		p.kvData = append(p.kvData, key...)
 		p.kvData = append(p.kvData, value...)
 		p.nodeData[node] = kvOffset
 		m := p.nodeData[node+nVal]
 		p.nodeData[node+nVal] = len(value)
-		p.kvSize += len(value) - m
+		p.kvSize += len(value) - m	// value长度变化值
 		return nil
 	}
 
@@ -306,8 +324,8 @@ func (p *DB) Put(key []byte, value []byte) error {
 	p.nodeData = append(p.nodeData, kvOffset, len(key), len(value), h)
 	for i, n := range p.prevNode[:h] {
 		m := n + nNext + i
-		p.nodeData = append(p.nodeData, p.nodeData[m])
-		p.nodeData[m] = node
+		p.nodeData = append(p.nodeData, p.nodeData[m])	// 设定新node的下一节
+		p.nodeData[m] = node	// 维护旧node下一节点
 	}
 
 	p.kvSize += len(key) + len(value)
@@ -319,6 +337,7 @@ func (p *DB) Put(key []byte, value []byte) error {
 // the DB does not contain the key.
 //
 // It is safe to modify the contents of the arguments after Delete returns.
+// 返回后可安全修改参数内容
 func (p *DB) Delete(key []byte) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
@@ -329,6 +348,7 @@ func (p *DB) Delete(key []byte) error {
 	}
 
 	h := p.nodeData[node+nHeight]
+	// 各层前一节指向要删除node的下一节点
 	for i, n := range p.prevNode[:h] {
 		m := n + nNext + i
 		p.nodeData[m] = p.nodeData[p.nodeData[m]+nNext+i]
@@ -342,6 +362,7 @@ func (p *DB) Delete(key []byte) error {
 // Contains returns true if the given key are in the DB.
 //
 // It is safe to modify the contents of the arguments after Contains returns.
+// 结果返回后可安全修改参数key
 func (p *DB) Contains(key []byte) bool {
 	p.mu.RLock()
 	_, exact := p.findGE(key, false)
@@ -354,10 +375,11 @@ func (p *DB) Contains(key []byte) bool {
 //
 // The caller should not modify the contents of the returned slice, but
 // it is safe to modify the contents of the argument after Get returns.
+// 不可修改返回的slice，但可修改传入的参数
 func (p *DB) Get(key []byte) (value []byte, err error) {
 	p.mu.RLock()
 	if node, exact := p.findGE(key, false); exact {
-		o := p.nodeData[node] + p.nodeData[node+nKey]
+		o := p.nodeData[node] + p.nodeData[node+nKey]	// 目标value在kvData起始位置
 		value = p.kvData[o : o+p.nodeData[node+nVal]]
 	} else {
 		err = ErrNotFound
@@ -372,6 +394,7 @@ func (p *DB) Get(key []byte) (value []byte, err error) {
 //
 // The caller should not modify the contents of the returned slice, but
 // it is safe to modify the contents of the argument after Find returns.
+// 返回大于等于给定key的键值对
 func (p *DB) Find(key []byte) (rkey, value []byte, err error) {
 	p.mu.RLock()
 	if node, _ := p.findGE(key, false); node != 0 {
@@ -398,13 +421,19 @@ func (p *DB) Find(key []byte) (rkey, value []byte, err error) {
 // DB. And a nil Range.Limit is treated as a key after all keys in
 // the DB.
 //
-// WARNING: Any slice returned by interator (e.g. slice returned by calling
+// WARNING: Any slice returned by iterator (e.g. slice returned by calling
 // Iterator.Key() or Iterator.Key() methods), its content should not be modified
 // unless noted otherwise.
 //
 // The iterator must be released after use, by calling Release method.
 //
 // Also read Iterator documentation of the leveldb/iterator package.
+// 单个并发不安全，但可同时在多个goroutine中使用不同迭代器
+// 在修改其基础数据库的同时并发使用迭代器也是安全的，但不能保证结果键/值对在特定时间点上是数据库的一致快照
+//
+// slice为迭代器设定范围，slice为nil表示所有key
+// iterator返回的任何slice，其内容不可修改，除非另有说明
+// iterator使用完后需释放
 func (p *DB) NewIterator(slice *util.Range) iterator.Iterator {
 	return &dbIter{p: p, slice: slice}
 }
@@ -419,6 +448,8 @@ func (p *DB) Capacity() int {
 // Size returns sum of keys and values length. Note that deleted
 // key/value will not be accounted for, but it will still consume
 // the buffer, since the buffer is append only.
+// 返回存储的键值对长度
+// 删除键值对的长度不在被记录，但仍在消耗实际buffer，
 func (p *DB) Size() int {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
@@ -426,6 +457,7 @@ func (p *DB) Size() int {
 }
 
 // Free returns keys/values free buffer before need to grow.
+// 返回可用容量
 func (p *DB) Free() int {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
@@ -433,6 +465,7 @@ func (p *DB) Free() int {
 }
 
 // Len returns the number of entries in the DB.
+// 返回存储的键值对数量
 func (p *DB) Len() int {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
@@ -440,6 +473,7 @@ func (p *DB) Len() int {
 }
 
 // Reset resets the DB to initial empty state. Allows reuse the buffer.
+// 初始化空状态，允许重用
 func (p *DB) Reset() {
 	p.mu.Lock()
 	p.rnd = rand.New(rand.NewSource(0xdeadbeef))
@@ -467,6 +501,7 @@ func (p *DB) Reset() {
 // reclaim KV buffer.
 //
 // The returned DB instance is safe for concurrent use.
+// 仅追加，删除的键值对不会从KV buffer中回收
 func New(cmp comparer.BasicComparer, capacity int) *DB {
 	p := &DB{
 		cmp:       cmp,
--- test.go
+++ test.go
@@ -1,3 +1,13 @@
 package main
 
+import "fmt"
 
+
+func main() {
+	b := []int{1,2,3,4,5,6}
+	fmt.Println(b)
+	b = append(b, b[2])
+	fmt.Println(b)
+	b[2] = 10
+	fmt.Println(b)
+}
